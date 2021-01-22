e0ce4c58c954268e81f17a3ad4c231f86c837fbf
Micha Alt <mail@lean-stack.de>
2021-01-22 12:10:26 +0100

    Implement hello-world sample.

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// Styles
+import './styles/styles.css';
+
+// React Hello World
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('root')
+);
