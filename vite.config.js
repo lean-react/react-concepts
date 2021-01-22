be429040f25aa80287656bff2c444550220fb503
Micha Alt <mail@lean-stack.de>
2021-01-22 12:09:50 +0100

    Add optional react refresh to vite.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default {
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    reactRefresh()
+  ]
 }
