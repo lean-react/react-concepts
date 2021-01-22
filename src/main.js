36194129c5f6a94a817fdb710d0fabfc34453e5b
Micha Alt <mail@lean-stack.de>
2021-01-22 11:10:53 +0100

    Add minimal toolchain via vite.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Styles
+import './styles/styles.css';
+
+// Vanilla ECMAScript App
+const heading = document.createElement('h1');
+heading.textContent = 'React Concepts';
+document.getElementById('root').appendChild(heading);
